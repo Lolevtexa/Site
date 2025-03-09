38415d375c78e9bef116ad40628a7afb337effc3
lolevtexa <ceba.kek@gmail.com>
2025-03-09 20:25:08 +0300

    вроде лучше, но хочу ещё лучше интерфейс

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('accounts/', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
